5f0bcc85ab4d8042d403e2c4e5eb637c7fc7bb57
olist213 <46209842+olist213@users.noreply.github.com>
2021-09-28 14:18:42 +0800

    更新第24天的内容统计学

---
+++ 第二期-python30天/day24 统计学（statistics）/day24_statistics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@文件    :day24_statistics.py
+@说明    :
+@时间    :2021/09/28 09:14:11
+@作者    :liangcheng
+@版本    :1.0
+'''
+
+import numpy as np
+
+
